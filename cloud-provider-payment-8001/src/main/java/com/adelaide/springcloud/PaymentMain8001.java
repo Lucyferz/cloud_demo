74225641ee1a4bafa0b401c571c12a682576318c
Wenhao Zhao <w.zhao@student.adelaide.edu.au>
2022-10-13 23:40:17 +1030

    update

--- cloud-provider-payment-8001/src/main/java/com/adelaide/springcloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/com/adelaide/springcloud/PaymentMain8001.java
@@ -2,6 +2,9 @@ package com.adelaide.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
--- cloud-provider-payment-8001/src/main/java/com/adelaide/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/adelaide/springcloud/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.adelaide.entities.CommonResult;
 import com.adelaide.entities.Payment;
 import com.adelaide.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,15 +16,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("Insertion result:" + result);
 
         if (result > 0) {
-            return new CommonResult(200, "Success insert data ", result);
+            return new CommonResult(200, "Success insert data, serverPort: " + serverPort, result);
         } else {
-            return new CommonResult(500, "Failed to insert data");
+            return new CommonResult(500, "Failed to insert data, serverPort: "  + serverPort);
         }
     }
 
@@ -35,7 +39,7 @@ public class PaymentController {
         if (payment==null) {
             return new CommonResult(500, "Payment record not found，id：　" + id);
         } else {
-            return new CommonResult(200, "Payment record founded", payment);
+            return new CommonResult(200, "Payment record founded, serverPort: "+ serverPort, payment);
         }
     }
 }
